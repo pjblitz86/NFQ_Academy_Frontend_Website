b2025e974a1c319230809780aec38c700499cf31
Paulius Juknis <paulius.juknis@gmail.com>
2018-09-11 14:19:27 +0300

    generated needed components via ng cli and added bootstrap navbar with search and made responsive with collapsing hamburger button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductsComponent } from './components/products/products.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+import { OrderListComponent } from './components/order-list/order-list.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductsListComponent,
+    ProductsComponent,
+    SidebarComponent,
+    AddProductComponent,
+    EditProductComponent,
+    OrderListComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    NotFoundComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
